5de85c6ba52bddc8afdf5385c36757de3ffd0ece
Game-Hook <mkk635865@gmail.com>
2025-04-23 05:44:48 +0300

    Update HookModEASY.cpp

--- HookModEASY.cpp
+++ HookModEASY.cpp
@@ -1,12 +1,21 @@
-
-//  فقط لنسخ 32 bit
+// فقط لنسخ 32 bit
+// For 32-bit systems only
 
 /* Variables for hook offset method */
-/* هوك المتعلق بالوفست */
+/* متغيرات لطريقة الهوك باستخدام الأوفست */
+
 //hook_name = bool mod = false;
+// hook_name => متغير من نوع bool لتفعيل الهوك أو تعطيله
+
 //hook_name_fake_func || اي اسم بالك
+// hook_name_fake_func => اسم الدالة الوهمية اللي ترجع القيمة المعدّلة
+
 //hook_name_point_func || اي اسم بالك بس كون يختلف عن اسم الاول
+// hook_name_point_func => مؤشر (pointer) إلى الدالة الأصلية لتحفض القيمه قبل تعديل
+
 //edit_Bool || قيمة تعديل (true or false)
+// edit_Bool => القيمة التي يتم إرجاعها إذا تم تفعيل الهوك
+
 
 // BOOL Hook
 #define ADD_BOOL_HOOK(hook_name, hook_name_point_func, hook_name_fake_func, edit_Bool) \
@@ -20,6 +29,7 @@ bool hook_name_fake_func(void *instance) { \
     return hook_name_point_func(instance); \
 }
 
+// هوك لنوع boolean (قيمة منطقية)
 
 
 // FLOAT Hook
@@ -35,6 +45,8 @@ float hook_name_fake_func(void *instance) { \
     return hook_name_point_func(instance); \
 }
 
+// هوك لنوع float (رقم عشري)
+
 
 // DOUBLE Hook
 /* Double type: */
@@ -49,6 +61,7 @@ double hook_name_fake_func(void *instance) { \
     return hook_name_point_func(instance); \
 }
 
+// هوك لنوع double (رقم عشري بدقة عالية)
 
 
 // INT Hook
@@ -64,17 +77,15 @@ int hook_name_fake_func(void *instance) { \
     return hook_name_point_func(instance); \
 }
 
+// هوك لنوع int (عدد صحيح)
+
+
 //كملنا من جميع الدوال التي تعمل مع اوفسيت
+// Done defining all functions that work with offset
 
 
-
-
-
-
-
-
-
-
+/* Hooking field offset method */
+/* طريقة هوك باستخدام أوفست الحقول (المتغيرات داخل الكائن) */
 
 
 
